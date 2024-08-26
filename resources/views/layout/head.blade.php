9d3db856f62ace713d0a5ca9270c8a30b8a32879
TSABIT BINTANG HERINDRA <222112402@stis.ac.id>
2024-08-26 22:33:55 +0700

    gatau bre

---
+++ resources/views/layout/head.blade.php
@@ -0,0 +1,30 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- ===============================================-->
+    <!--    Document Title-->
+    <!-- ===============================================-->
+    <title>Portal BPS Kabupaten Bogor</title>
+
+    <!-- ===============================================-->
+    <!--    Favicons-->
+    <!-- ===============================================-->
+    <link rel="icon" sizes="180x180" href="{{url('assets/img/gallery/logobps.png')}}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{url('assets/img/gallery/logobps.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{url('assets/img/gallery/logobps.png')}}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{url('assets/img/favicons/favicon.ico')}}">
+    <link rel="manifest" href="{{url('assets/img/favicons/manifest.json')}}">
+    <meta name="msapplication-TileImage" content="{{url('assets/img/favicons/mstile-150x150.png')}}">
+    <meta name="theme-color" content="#ffffff">
+
+    <!-- ===============================================-->
+    <!--    Stylesheets-->
+    <!-- ===============================================-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Baloo+Bhaijaan+2:wght@400;500;600;700&amp;family=Poppins:ital,wght@0,400;0,500;0,600;0,700;1,300&amp;display=swap" rel="stylesheet">
+    <link href="{{url('assets/css/theme.min.css')}}" rel="stylesheet" />
+    <link href="{{url('assets/css/user.min.css')}}" rel="stylesheet" />
+  </head>
---
+++ resources/views/layout/navbar.blade.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" data-navbar-on-scroll="light">
+    <div class="container"><a class="navbar-brand" href="index.html"><img src="{{url('assets/img/gallery/logobpsbogor_dark.png')}}" height="40" alt="logo" /></a><button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+      <div class="collapse navbar-collapse border-top border-lg-0 mt-4 mt-lg-0" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto pt-2 pt-lg-0 font-base align-items-center">
+          <li class="nav-item"><a class="nav-link px-3" href="#home">Home</a></li>
+          <li class="nav-item"><a class="nav-link px-3" href="#tentangkami">About Us</a></li>
+          <li class="nav-item"><a class="nav-link px-3" href="#inovasi">Inovation</a></li>
+          <li class="nav-item"><a class="nav-link pl-3 me-3" href="#storage">Storage</a></li>
+          <li class="nav-item"><a class="nav-link pl-3 me-3" href="#official">Official</a></li>
+        </ul><button class="btn btn-primary">Sign Up</button>
+      </div>
+    </div>
+  </nav>
