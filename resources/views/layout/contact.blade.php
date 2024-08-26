dc8d540f546fc414a60d90f22df8e2895f9827e2
Gilang Abdul <222112076@stis.ac.id>
2024-08-26 22:00:52 +0700

    ubah footer

--- resources/views/layout/contact.blade.php
+++ resources/views/layout/contact.blade.php
@@ -6,26 +6,26 @@
                 <div class="col-lg-6">
                     <h2 class="fw-bold text-black">Pelayanan Statistik Terpadu</h2>
                     <p>Pelayanan data kini dapat hadir secara langsung!</p>
+                    <div class="input-group">
+                        <input class="form-control" type="text" placeholder="Email Address" />
+                        <button class="btn btn-primary rounded-start-0 px-3" type="submit">Get Started</button>
+                    </div>
+                </div>
+                <div class="col-lg-6">
                     <div class="h-20">
                         <!-- Responsive Google Maps iframe -->
-                        <div class="embed-responsive embed-responsive-16by9">
-                            <iframe class="embed-responsive-item"
+                        <div class="embed-responsive embed-responsive-16by9 mt-3">
+                            <iframe class="embed-responsive-item ml-5"
                                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.3387744204447!2d106.82044087531362!3d-6.478706493513148!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69c1f3ff546b7f%3A0x7ea97e621b50da03!2sBPS%20Kabupaten%20Bogor%20(Badan%20Pusat%20Statistik)!5e0!3m2!1sid!2sid!4v1724340102983!5m2!1sid!2sid"
-                                style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+                                style="border:0;width:max-content" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
                             </iframe>
                         </div>
                         <h5 class="mt-4 mb-2">BPS Kabupaten Bogor</h5>
                         <p>Pemda Cibinong Jalan Bersih, Kompleks Perkantoran, Tengah, Kec. Cibinong, Kabupaten Bogor, Jawa Barat <br> 16914</p>
                     </div>
                 </div>
-                <div class="col-lg-6 h-100">
-                    <div class="input-group"> 
-                        <input class="form-control" type="text" placeholder="Email Address" />
-                        <button class="btn btn-primary rounded-start-0 px-3" type="submit">Get Started</button>
-                    </div>
-                </div>
-            </div><!-- end of .container-->
+            </div>
+            <!-- end of .container-->
         </div>
     </section><!-- <section> close ============================-->
     <!-- ============================================-->
-    
--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,40 +1,40 @@
 <section class="py-0 bg-1000">
     <div class="container">
-      <div class="row py-7">
-        <div class="col-12 col-sm-12 col-lg-6 mb-4 order-0 order-sm-0"><a class="text-decoration-none" href="#"><img src="{{url('assets/img/gallery/logobpsbogor_light.png')}}" height="51" alt="" /></a>
+      <div class="row py-5 justify-content-between">
+        <div class="col-4 col-sm-12 col-lg-6 mb-4"><a class="text-decoration-none" href="#"><img src="{{url('assets/img/gallery/logobpsbogor_light.png')}}" height="51" alt="" /></a>
           <p class="text-light mt-4 mb-0">Penyedia Layanan Statistik Terpadu</p>
           <p class="text-light mb-0">Kabupaten Bogor, Jawa Barat</p>
           <p class="text-light mt-5">bps3201@bps.go.id</p>
-          <div class="d-flex"><a href="#!"><img class="me-xl-3 me-1" src="assets/img/gallery/linkedin-line1.svg" alt="in" width:'20px'="width:'20px'" height:'20px'="height:'20px'" /></a><a href="#!"><img class="me-xl-3 me-1" src="assets/img/gallery/facebook-line1.svg" alt="fb" width:'20px'="width:'20px'" height:'20px'="height:'20px'" /></a><a href="#!"><img class="me-xl-3 me-1" src="assets/img/gallery/twitter-line1.svg" alt="twitter" width:'20px'="width:'20px'" height:'20px'="height:'20px'" /></a><a href="#!"><img class="me-xl-3 me-1" src="assets/img/gallery/instagram-line1.svg" alt="instragram" width:'20px'="width:'20px'" height:'20px'="height:'20px'" /></a></div>
+          <div class="d-flex">
+            <a href="#!"><img class="me-xl-3 me-1" src="assets/img/gallery/linkedin-line1.svg" alt="in" width:'20px'="width:'20px'" height:'20px'="height:'20px'" /></a>
+            <a href="#!"><img class="me-xl-3 me-1" src="assets/img/gallery/facebook-line1.svg" alt="fb" width:'20px'="width:'20px'" height:'20px'="height:'20px'" /></a>
+            <a href="#!"><img class="me-xl-3 me-1" src="assets/img/gallery/twitter-line1.svg" alt="twitter" width:'20px'="width:'20px'" height:'20px'="height:'20px'" /></a>
+            <a href="#!"><img class="me-xl-3 me-1" src="assets/img/gallery/instagram-line1.svg" alt="instragram" width:'20px'="width:'20px'" height:'20px'="height:'20px'" /></a>
+            </div>
         </div>
-        <div class="col-3 col-sm-4 col-lg-2 mb-3 order-2 order-sm-1">
-          <h5 class="lh-lg fw-bold text-light font-sans-serif">Layanan Lainnya</h5>
-          <ul class="list-unstyled text-light mb-md-4">
-            <li class="lh-lg"><a class="text-500 footer-link text-light" href="https://webapi.bps.go.id/developer/?_gl=1*zlqdoj*_ga*NzI2MTgwNTE0LjE2OTI5NDU3ODM.*_ga_XXTTVXWHDB*MTcyNDYzODUwMy41MS4wLjE3MjQ2Mzg1MDguMC4wLjA.">WebAPI BPS</a></li>
-            <li class="lh-lg"><a class="text-500 footer-link text-light" href="https://indah.bps.go.id/?_gl=1%2azlqdoj%2a_ga%2aNzI2MTgwNTE0LjE2OTI5NDU3ODM.%2a_ga_XXTTVXWHDB%2aMTcyNDYzODUwMy41MS4wLjE3MjQ2Mzg1MDguMC4wLjA.">INDAH</a></li>
-            <li class="lh-lg"><a class="text-500 footer-link text-light" href="https://webapi.bps.go.id/developer/?_gl=1*c8qr3h*_ga*NzI2MTgwNTE0LjE2OTI5NDU3ODM.*_ga_XXTTVXWHDB*MTcyNDYzODUwMy41MS4wLjE3MjQ2Mzg1MDguMC4wLjA.">API Geoportal</a></li>
-            <li class="lh-lg"><a class="text-500 footer-link text-light" href="https://klasifikasi.web.bps.go.id/?_gl=1*1user4m*_ga*NzI2MTgwNTE0LjE2OTI5NDU3ODM.*_ga_XXTTVXWHDB*MTcyNDYzODUwMy41MS4wLjE3MjQ2Mzg1MDguMC4wLjA.">KBLI KBKI</a></li>
-            <li class="lh-lg"><a class="text-500 footer-link text-light" href="https://bogorkab.bps.go.id/id/query-builder">Tabel Dinamis</a></li>
-            <li class="lh-lg"><a class="text-500 footer-link text-light" href="https://bogorkab.bps.go.id/id/publication">Publikasi</a></li>
-          </ul>
-        </div>
-        <div class="col-6 col-sm-4 col-lg-2 mb-3 order-3 order-sm-2">
-          <h5 class="lh-lg fw-bold text-light font-sans-serif">Offerings</h5>
-          <ul class="list-unstyled text-light mb-md-4 mb-lg-0">
-            <li class="lh-lg"><a class="text-500 footer-link text-light" href="#!">Gohub Eats</a></li>
-            <li class="lh-lg"><a class="text-500 footer-link text-light" href="#!">Gohub Business</a></li>
-            <li class="lh-lg"><a class="text-500 footer-link text-light" href="#!">Guhub Freight</a></li>
-            <li class="lh-lg"><a class="text-500 footer-link text-light" href="#!">Gohub Health</a></li>
-          </ul>
-        </div>
-        <div class="col-6 col-sm-4 col-lg-2 mb-3 order-3 order-sm-2">
-          <h5 class="lh-lg fw-bold text-light font-sans-serif"> Support</h5>
-          <ul class="list-unstyled text-light mb-md-4 mb-lg-0">
-            <li class="lh-lg"><a class="text-500 footer-link text-light" href="#!">Terminal Conditions</a></li>
-            <li class="lh-lg"><a class="text-500 footer-link text-light" href="#!">About Us</a></li>
-            <li class="lh-lg"><a class="text-500 footer-link text-light" href="#!">Contact US</a></li>
-            <li class="lh-lg"><a class="text-500 footer-link text-light" href="#!">Terms of Use</a></li>
-          </ul>
+        <div class="col-4">
+            <div class="row justify-content-end">
+                <div class="col- col-sm-6 col-lg-6 mb-3">
+                    <h5 class="lh-lg fw-bold text-light font-sans-serif">Offerings</h5>
+                    <ul class="list-unstyled text-light mb-md-4 mb-lg-0">
+                    <li class="lh-lg"><a class="text-500 footer-link text-light" href="#!">Gohub Eats</a></li>
+                    <li class="lh-lg"><a class="text-500 footer-link text-light" href="#!">Gohub Business</a></li>
+                    <li class="lh-lg"><a class="text-500 footer-link text-light" href="#!">Guhub Freight</a></li>
+                    <li class="lh-lg"><a class="text-500 footer-link text-light" href="#!">Gohub Health</a></li>
+                    </ul>
+                </div>
+                <div class="col- col-sm-6 col-lg-6 mb-3">
+                    <h5 class="lh-lg fw-bold text-light font-sans-serif">Layanan Lainnya</h5>
+                    <ul class="list-unstyled text-light mb-md-4">
+                    <li class="lh-lg"><a class="text-500 footer-link text-light" href="https://webapi.bps.go.id/developer/?_gl=1*zlqdoj*_ga*NzI2MTgwNTE0LjE2OTI5NDU3ODM.*_ga_XXTTVXWHDB*MTcyNDYzODUwMy41MS4wLjE3MjQ2Mzg1MDguMC4wLjA.">WebAPI BPS</a></li>
+                    <li class="lh-lg"><a class="text-500 footer-link text-light" href="https://indah.bps.go.id/?_gl=1%2azlqdoj%2a_ga%2aNzI2MTgwNTE0LjE2OTI5NDU3ODM.%2a_ga_XXTTVXWHDB%2aMTcyNDYzODUwMy41MS4wLjE3MjQ2Mzg1MDguMC4wLjA.">INDAH</a></li>
+                    <li class="lh-lg"><a class="text-500 footer-link text-light" href="https://webapi.bps.go.id/developer/?_gl=1*c8qr3h*_ga*NzI2MTgwNTE0LjE2OTI5NDU3ODM.*_ga_XXTTVXWHDB*MTcyNDYzODUwMy41MS4wLjE3MjQ2Mzg1MDguMC4wLjA.">API Geoportal</a></li>
+                    <li class="lh-lg"><a class="text-500 footer-link text-light" href="https://klasifikasi.web.bps.go.id/?_gl=1*1user4m*_ga*NzI2MTgwNTE0LjE2OTI5NDU3ODM.*_ga_XXTTVXWHDB*MTcyNDYzODUwMy41MS4wLjE3MjQ2Mzg1MDguMC4wLjA.">KBLI KBKI</a></li>
+                    <li class="lh-lg"><a class="text-500 footer-link text-light" href="https://bogorkab.bps.go.id/id/query-builder">Tabel Dinamis</a></li>
+                    <li class="lh-lg"><a class="text-500 footer-link text-light" href="https://bogorkab.bps.go.id/id/publication">Publikasi</a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
       </div>
     </div>
@@ -44,13 +44,8 @@
     <section class="py-0 bg-1000">
       <div class="container">
         <div class="row justify-content-md-between justify-content-evenly py-3">
-          <div class="col-12 col-sm-8 col-md-6 col-lg-auto text-center text-md-start">
-            <p class="fs--1 my-2 fw-bold text-200">&copy; 2022 Your Company Inc</p>
-          </div>
-          <div class="col-12 col-sm-8 col-md-6">
-            <p class="fs--1 my-2 text-center text-md-end text-200"> Made with&nbsp;<svg class="bi bi-suit-heart-fill" xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="#F95C19" viewBox="0 0 16 16">
-                <path d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z"></path>
-              </svg>&nbsp;by&nbsp;<a class="fw-bold text-info text-200" href="https://themewagon.com/" target="_blank">ThemeWagon </a></p>
+          <div class="col-12 col-sm-8 col-md-6 col-lg-auto text-center">
+            <p class="fs--1 my-2 fw-bold text-200">&copy; BPS Kabupaten Bogor 2024</p>
           </div>
         </div>
       </div><!-- end of .container-->
