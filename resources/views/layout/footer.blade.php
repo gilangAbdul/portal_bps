8b245ec1a15194c6c64db2f3d0da8d20193034d4
AFI DWI AMINURRAHMAH <222111849@stis.ac.id>
2024-08-26 15:39:42 +0700

    Responsif

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -8,14 +8,14 @@
           <div class="d-flex"><a href="#!"><img class="me-xl-3 me-1" src="assets/img/gallery/linkedin-line1.svg" alt="in" width:'20px'="width:'20px'" height:'20px'="height:'20px'" /></a><a href="#!"><img class="me-xl-3 me-1" src="assets/img/gallery/facebook-line1.svg" alt="fb" width:'20px'="width:'20px'" height:'20px'="height:'20px'" /></a><a href="#!"><img class="me-xl-3 me-1" src="assets/img/gallery/twitter-line1.svg" alt="twitter" width:'20px'="width:'20px'" height:'20px'="height:'20px'" /></a><a href="#!"><img class="me-xl-3 me-1" src="assets/img/gallery/instagram-line1.svg" alt="instragram" width:'20px'="width:'20px'" height:'20px'="height:'20px'" /></a></div>
         </div>
         <div class="col-3 col-sm-4 col-lg-2 mb-3 order-2 order-sm-1">
-          <h5 class="lh-lg fw-bold text-light font-sans-serif">About Us</h5>
+          <h5 class="lh-lg fw-bold text-light font-sans-serif">Layanan Lainnya</h5>
           <ul class="list-unstyled text-light mb-md-4">
-            <li class="lh-lg"><a class="text-500 footer-link text-light" href="#!">Newsroom</a></li>
-            <li class="lh-lg"><a class="text-500 footer-link text-light" href="#!">Blog</a></li>
-            <li class="lh-lg"><a class="text-500 footer-link text-light" href="#!">Gohub API</a></li>
-            <li class="lh-lg"><a class="text-500 footer-link text-light" href="#!">Gift cards</a></li>
-            <li class="lh-lg"><a class="text-500 footer-link text-light" href="#!">Career</a></li>
-            <li class="lh-lg"><a class="text-500 footer-link text-light" href="#!">Gohub driving jobs</a></li>
+            <li class="lh-lg"><a class="text-500 footer-link text-light" href="https://webapi.bps.go.id/developer/?_gl=1*zlqdoj*_ga*NzI2MTgwNTE0LjE2OTI5NDU3ODM.*_ga_XXTTVXWHDB*MTcyNDYzODUwMy41MS4wLjE3MjQ2Mzg1MDguMC4wLjA.">WebAPI BPS</a></li>
+            <li class="lh-lg"><a class="text-500 footer-link text-light" href="https://indah.bps.go.id/?_gl=1%2azlqdoj%2a_ga%2aNzI2MTgwNTE0LjE2OTI5NDU3ODM.%2a_ga_XXTTVXWHDB%2aMTcyNDYzODUwMy41MS4wLjE3MjQ2Mzg1MDguMC4wLjA.">INDAH</a></li>
+            <li class="lh-lg"><a class="text-500 footer-link text-light" href="https://webapi.bps.go.id/developer/?_gl=1*c8qr3h*_ga*NzI2MTgwNTE0LjE2OTI5NDU3ODM.*_ga_XXTTVXWHDB*MTcyNDYzODUwMy41MS4wLjE3MjQ2Mzg1MDguMC4wLjA.">API Geoportal</a></li>
+            <li class="lh-lg"><a class="text-500 footer-link text-light" href="https://klasifikasi.web.bps.go.id/?_gl=1*1user4m*_ga*NzI2MTgwNTE0LjE2OTI5NDU3ODM.*_ga_XXTTVXWHDB*MTcyNDYzODUwMy41MS4wLjE3MjQ2Mzg1MDguMC4wLjA.">KBLI KBKI</a></li>
+            <li class="lh-lg"><a class="text-500 footer-link text-light" href="https://bogorkab.bps.go.id/id/query-builder">Tabel Dinamis</a></li>
+            <li class="lh-lg"><a class="text-500 footer-link text-light" href="https://bogorkab.bps.go.id/id/publication">Publikasi</a></li>
           </ul>
         </div>
         <div class="col-6 col-sm-4 col-lg-2 mb-3 order-3 order-sm-2">
